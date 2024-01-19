303c8926940bb299ca0566d18fdbfb4c638b7846
Mustapha Moumanis <mmoumani@e1r1p11.1337.ma>
2024-01-19 20:23:19 +0100

    add wp-config and install wordpress

--- Makefile
+++ Makefile
@@ -1,2 +1,16 @@
-all:
-	docker
+up: build
+	cd srcs; docker-compose up
+
+build:
+	cd srcs; docker-compose build
+
+down:
+	cd srcs; docker-compose down
+
+clean:
+	cd srcs; docker-compose down; docker volume prune -f
+# docker image rm -f $$(docker images -aq)
+
+fclean: clean
+
+re: clean up
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -15,6 +15,16 @@
 #         echo "WAITING..."
 #     fi
 # done
-wp core download
+
+# wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# chmod +x wp-cli.phar
+# mv wp-cli.phar /usr/local/bin/wp
+
+# cd /var/www/html ; wp core download --allow-root
+sleep 3
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$HOST --allow-root
+wp core install --url=$DOMAIN_NAME --title=$WP_WEB_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_EMAIL --allow-root
+wp user create $WP_USER $WP_USER_EMAIL --role=subscriber --user_pass=$WP_USER_PASSWORD --allow-root
+wp theme install twentytwenty --activate --allow-root
 
 php-fpm8.2 -F
