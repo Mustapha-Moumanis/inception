5a535477a93b6f7527409ac8014cf6477f452d6c
Mustapha Moumanis <mmoumani@e1r1p11.1337.ma>
2024-01-24 18:40:03 +0100

    update domain name

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-echo "$(sed 's/localhost/mmoumani.42.fr/g' etc/hosts)" > etc/hosts
-
 nginx -g "daemon off;"
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,7 +6,7 @@ wordpress_config() {
     done
     # sleep 5
     wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --allow-root
-    wp core install --url=$DOMAIN_NAME --title=$WP_WEB_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_EMAIL --allow-root
+    wp core install --url=$DOMAIN_NAME --title=$WP_WEB_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
     wp user create $WP_USER $WP_USER_EMAIL --role=subscriber --user_pass=$WP_USER_PASSWORD --allow-root
     wp theme install twentytwenty --activate --allow-root
 }
