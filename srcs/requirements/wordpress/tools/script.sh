a4fb173be711da5f44fbf8a92f4f7e6cc699fcaf
Mustapha Moumanis <mmoumani@e1r2p12.1337.ma>
2024-02-04 20:19:03 +0100

    update ports

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -10,7 +10,7 @@ wordpress_config() {
         wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --allow-root
         wp core install --url=$DOMAIN_NAME --title=$WP_WEB_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
         wp user create $WP_USER $WP_USER_EMAIL --role=subscriber --user_pass=$WP_USER_PASSWORD --allow-root
-        wp theme install twentytwenty --activate --allow-root
+        # wp theme install twentytwenty --activate --allow-root
     fi
 }
 
