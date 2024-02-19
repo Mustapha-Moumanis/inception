c4b74e52c779b73624d074306d58998ffdec4941
Mustapha Moumanis <mmoumani@c4r8p4.1337.ma>
2024-02-19 21:13:39 +0100

    add check if certificate is exist or not

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout $P_KEY \
-    -out $CERTS_ -subj "/CN=$DOMAIN_NAME"
+if ! [ -f $P_KEY && -f $CERTS_ ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout $P_KEY \
+        -out $CERTS_ -subj "/CN=$DOMAIN_NAME" 2&>/dev/null
+    echo "Certificate and private key have been generated!"
+else
+    echo "There is already a certificate and private key!"
+fi
 
 sed -i -e "s+DOMAIN_NAME+$DOMAIN_NAME+" -e "s+CERTS_+$CERTS_+" -e "s+P_KEY+$P_KEY+" /etc/nginx/sites-available/default
 
